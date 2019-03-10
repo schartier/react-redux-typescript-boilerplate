ff7f0c8d4b5e5f2a710f14ff90e55d35fe08e81c
Sebastien Chartier <schartier@landr.com>
2019-03-10 13:41:56 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-var webpack = require('webpack');
-var path = require('path');
-var package = require('./package.json');
+const webpack = require('webpack');
+const path = require('path');
+const package = require('./package.json');
 
 // variables
-var isProduction = process.argv.indexOf('-p') >= 0 || process.env.NODE_ENV === 'production';
-var sourcePath = path.join(__dirname, './src');
-var outPath = path.join(__dirname, './build');
+const isProduction = process.argv.indexOf('-p') >= 0 || process.env.NODE_ENV === 'production';
+const sourcePath = path.join(__dirname, './src');
+const outPath = path.join(__dirname, './build');
 
 // plugins
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var MiniCssExtractPlugin = require('mini-css-extract-plugin');
-var WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
 
 module.exports = {
   context: sourcePath,
   entry: {
-    app: './main.tsx'
+    app: './main.tsx',
   },
   output: {
     path: outPath,
     filename: isProduction ? '[contenthash].js' : '[hash].js',
-    chunkFilename: isProduction ? '[name].[contenthash].js' : '[name].[hash].js'
+    chunkFilename: isProduction ? '[name].[contenthash].js' : '[name].[hash].js',
   },
   target: 'web',
   resolve: {
@@ -29,8 +29,8 @@ module.exports = {
     // (jsnext:main directs not usually distributable es6 format, but es6 sources)
     mainFields: ['module', 'browser', 'main'],
     alias: {
-      app: path.resolve(__dirname, 'src/app/')
-    }
+      app: path.resolve(__dirname, 'src/app/'),
+    },
   },
   module: {
     rules: [
@@ -40,10 +40,11 @@ module.exports = {
         use: [
           !isProduction && {
             loader: 'babel-loader',
-            options: { plugins: ['react-hot-loader/babel'] }
+            options: { plugins: ['react-hot-loader/babel'] },
           },
-          'ts-loader'
-        ].filter(Boolean)
+          'ts-loader',
+          'eslint-loader',
+        ].filter(Boolean),
       },
       // scss
       {
@@ -56,8 +57,8 @@ module.exports = {
               modules: true,
               sourceMap: !isProduction,
               importLoaders: 1,
-              localIdentName: isProduction ? '[hash:base64:5]' : '[local]__[hash:base64:5]'
-            }
+              localIdentName: isProduction ? '[hash:base64:5]' : '[local]__[hash:base64:5]',
+            },
           },
           {
             loader: 'postcss-loader',
@@ -68,17 +69,17 @@ module.exports = {
                 require('postcss-url')(),
                 require('postcss-preset-env')({
                   /* use stage 2 features (defaults) */
-                  stage: 2
+                  stage: 2,
                 }),
                 require('postcss-reporter')(),
                 require('postcss-browser-reporter')({
-                  disabled: isProduction
-                })
-              ]
-            }
+                  disabled: isProduction,
+                }),
+              ],
+            },
           },
-          "sass-loader" // compiles Sass to CSS, using Node Sass by default
-        ]
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
       },
       // css
       {
@@ -91,8 +92,8 @@ module.exports = {
               modules: true,
               sourceMap: !isProduction,
               importLoaders: 1,
-              localIdentName: isProduction ? '[hash:base64:5]' : '[local]__[hash:base64:5]'
-            }
+              localIdentName: isProduction ? '[hash:base64:5]' : '[local]__[hash:base64:5]',
+            },
           },
           {
             loader: 'postcss-loader',
@@ -103,25 +104,25 @@ module.exports = {
                 require('postcss-url')(),
                 require('postcss-preset-env')({
                   /* use stage 2 features (defaults) */
-                  stage: 2
+                  stage: 2,
                 }),
                 require('postcss-reporter')(),
                 require('postcss-browser-reporter')({
-                  disabled: isProduction
-                })
-              ]
-            }
-          }
-        ]
+                  disabled: isProduction,
+                }),
+              ],
+            },
+          },
+        ],
       },
       // static assets
       { test: /\.html$/, use: 'html-loader' },
       { test: /\.(a?png|svg)$/, use: 'url-loader?limit=10000' },
       {
         test: /\.(jpe?g|gif|bmp|mp3|mp4|ogg|wav|eot|ttf|woff|woff2)$/,
-        use: 'file-loader'
-      }
-    ]
+        use: 'file-loader',
+      },
+    ],
   },
   optimization: {
     splitChunks: {
@@ -129,27 +130,27 @@ module.exports = {
       cacheGroups: {
         commons: {
           chunks: 'initial',
-          minChunks: 2
+          minChunks: 2,
         },
         vendors: {
           test: /[\\/]node_modules[\\/]/,
           chunks: 'all',
           filename: isProduction ? 'vendor.[contenthash].js' : 'vendor.[hash].js',
-          priority: -10
-        }
-      }
+          priority: -10,
+        },
+      },
     },
-    runtimeChunk: true
+    runtimeChunk: true,
   },
   plugins: [
     new webpack.EnvironmentPlugin({
       NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
-      DEBUG: false
+      DEBUG: false,
     }),
     new WebpackCleanupPlugin(),
     new MiniCssExtractPlugin({
       filename: '[hash].css',
-      disable: !isProduction
+      disable: !isProduction,
     }),
     new HtmlWebpackPlugin({
       template: 'assets/index.html',
@@ -159,27 +160,27 @@ module.exports = {
         removeComments: true,
         useShortDoctype: true,
         collapseWhitespace: true,
-        collapseInlineTagWhitespace: true
+        collapseInlineTagWhitespace: true,
       },
       append: {
-        head: `<script src="//cdn.polyfill.io/v3/polyfill.min.js"></script>`
+        head: `<script src="//cdn.polyfill.io/v3/polyfill.min.js"></script>`,
       },
       meta: {
         title: package.name,
         description: package.description,
-        keywords: Array.isArray(package.keywords) ? package.keywords.join(',') : undefined
-      }
-    })
+        keywords: Array.isArray(package.keywords) ? package.keywords.join(',') : undefined,
+      },
+    }),
   ],
   devServer: {
     contentBase: sourcePath,
     hot: true,
     inline: true,
     historyApiFallback: {
-      disableDotRule: true
+      disableDotRule: true,
     },
     stats: 'minimal',
-    clientLogLevel: 'warning'
+    clientLogLevel: 'warning',
   },
   // https://webpack.js.org/configuration/devtool/
   devtool: isProduction ? 'hidden-source-map' : 'cheap-module-eval-source-map',
@@ -187,6 +188,6 @@ module.exports = {
     // workaround for webpack-dev-server issue
     // https://github.com/webpack/webpack-dev-server/issues/60#issuecomment-103411179
     fs: 'empty',
-    net: 'empty'
-  }
+    net: 'empty',
+  },
 };
